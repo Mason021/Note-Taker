9745f8546afed9c3bc8a9203fc8541d31fdd91a7
Mason021 <mdc5054@gmail.com>
2021-08-08 14:54:40 -0500

    Add Heroku

--- routes/routesOfAPI.js
+++ routes/routesOfAPI.js
@@ -2,15 +2,15 @@
 const fs = require("fs");
 const noteInfo = require("../db/db.json");
 
-module.exports = function(application){
+module.exports = function (application) {
 
 
-    function writeInfoToDB(notes){
+    function writeInfoToDB(notes) {
         // Converts new JSON Array back to string
         notes = JSON.stringify(notes);
-        console.log (notes);
+        console.log(notes);
         // Writes String back to db.json
-        fs.writeFileSync("./db/db.json", notes, function(err){
+        fs.writeFileSync("./db/db.json", notes, function (err) {
             if (err) {
                 return console.log(err);
             }
@@ -19,20 +19,20 @@ module.exports = function(application){
 
 
     // GET Method to return all the notes
-    application.get("/api/notes", function(req, res){
+    application.get("/api/notes", function (req, res) {
         res.json(noteInfo);
     });
 
     // POST Method to add to the notes
-    application.post("/api/notes", function(req, res){
+    application.post("/api/notes", function (req, res) {
 
         // Set unique id to entry
-        if (noteInfo.length == 0){
+        if (noteInfo.length == 0) {
             req.body.id = "0";
-        } else{
+        } else {
             req.body.id = JSON.stringify(JSON.parse(noteInfo[noteInfo.length - 1].id) + 1);
         }
-        
+
         console.log("req.body.id: " + req.body.id);
 
         // Pushes Body info to JSON Array
@@ -44,6 +44,28 @@ module.exports = function(application){
 
         // returns a new note in JSON format.
         res.json(req.body);
+    });
+
+    // DELETE Method to delete note with specified ID
+    application.delete("/api/notes/:id", function (req, res) {
+
+        // Obtains id and converts to a string
+        let id = req.params.id.toString();
+        console.log(id);
+
+        // Goes through notesArray searching for matching ID
+        for (i = 0; i < noteInfo.length; i++) {
+
+            if (noteInfo[i].id == id) {
+                console.log("match!");
+                // responds with deleted note
+                res.send(noteInfo[i]);
+
+                // Removes the deleted note
+                noteInfo.splice(i, 1);
+                break;
+            }
+        }
 
 
         // Write notes data to database
