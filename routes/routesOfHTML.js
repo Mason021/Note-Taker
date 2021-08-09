43737ec12566c756c5a3c0e78100d48156d21d84
Mason021 <mdc5054@gmail.com>
2021-08-09 00:27:04 -0500

    formated the html route js file

--- routes/routesOfHTML.js
+++ routes/routesOfHTML.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
-module.exports = function(app){
-    
+module.exports = function (app) {
+
     // Route to direct to the notes page
-    app.get("/notes", function(req, res){
+    app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     // Route to direct to the index page
-    app.get("/", function(req, res){
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 }
